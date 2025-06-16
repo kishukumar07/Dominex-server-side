052ddd89da9ce99836dbc3d09df0ed664e0413a4
kishukumar07 <kishukumars082@gmail.com>
2025-06-16 19:56:22 +0530

    update info and user password route built

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,15 @@
-import UserModel from '../models/user.models.model.js'
+import UserModel from "../models/user.models.model.js";
 
-
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
 
 const userProfile = async (req, res) => {
   const userid = req.params.id;
-// console.log(req.params)
+  // console.log(req.params)
 
   if (!mongoose.Types.ObjectId.isValid(userid)) {
     return res.status(400).json({
       success: false,
-      msg: "Invalid user ID format"
+      msg: "Invalid user ID format",
     });
   }
 
@@ -20,7 +18,7 @@ const userProfile = async (req, res) => {
       name: 1,
       username: 1,
       bio: 1,
-      email:1, 
+      email: 1,
       profilePic: 1,
       bannerPic: 1,
       isVerified: 1,
@@ -29,33 +27,153 @@ const userProfile = async (req, res) => {
       websiteUrl: 1,
       followers: 1,
       followings: 1,
-      posts: 1
-    })
-      // .populate('followers', 'username profilePic')
-      // .populate('followings', 'username profilePic')
-      // .populate('posts');
+      posts: 1,
+    });
+    // .populate('followers', 'username profilePic')
+    // .populate('followings', 'username profilePic')
+    // .populate('posts');
 
     if (!user) {
       return res.status(404).json({
         success: false,
-        msg: "User doesn't exist"
+        msg: "User doesn't exist",
       });
     }
 
     return res.status(200).json({
       success: true,
       msg: "User profile fetched successfully",
-      data: user
+      data: user,
     });
-
   } catch (error) {
     // console.error("Error fetching user profile:", error);
     return res.status(500).json({
       success: false,
       msg: "Server error while fetching user profile",
-      error: error.message
+      error: error.message,
     });
   }
+};
+
+const updateUserInfo = async (req, res) => {
+  //also authorization check here ...
+  const u_id = req.userId;
+  const givenUserid = req.params.id;
+
+  if (u_id !== givenUserid) {
+    return res.status(403).json({
+      success: false,
+      msg: "You are not authorized to update this user's information",
+    });
+  }
+
+  try {
+    const data = req.body;
+
+    const allowedFields = [
+      "name",
+      "username",
+      "bio",
+      "profilePic",
+      "bannerPic",
+      "websiteUrl",
+      "gender",
+      "dateOfBirth",
+    ];
+    // Filter only allowed fields
+    const updates = {};
+    for (let key of allowedFields) {
+      if (req.body[key] !== undefined) {
+        updates[key] = req.body[key];
+      }
+    }
+    const user = await UserModel.findByIdAndUpdate(
+      u_id,
+      { ...updates },
+      { new: true, runValidators: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      msg: "User information updated successfully",
+      data: user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg: "Server error while updating user information",
+      error: err.message,
+    });
+  }
+};
+
+const updatePassword = async (req, res) => {
+  // Old password verification required
+  // Secure password hashing
+  // Cannot be used to update anything else
+
+  const givenUserid = req.params.id;
+  const u_id = req.userId;
+  if (givenUserid !== givenUserid) {
+    return res.status(403).json({
+      success: false,
+      msg: "You are not authorized to change this user's password",
+    });
+  }
+
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || newPassword) {
+    return res.status(400).json({
+      success: false,
+      msg: "Both old and new passwords are required",
+    });
+  }
+
+  try {
+    const user = await UserModel.findById(u_id, {
+      password: 1,
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        msg: "user not found",
+      });
+    }
+
+    const isValidPassword = user.comparePassword(oldPassword);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Incorrect Password" });
+    }
+
+    user.password = newPassword;
+    await user.save();
+
+    res.status(200).json({
+      sucess: true,
+      msg: "password updated sucessfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg: "Server error while Updating the password ",
+      error: err.message,
+    });
+  }
+};
+
+const resetPassword = (req, res) => {
+
+  
 };
 
 
@@ -63,19 +181,4 @@ const userProfile = async (req, res) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-export {
-userProfile 
-}
+export { userProfile, updateUserInfo, updatePassword, resetPassword };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,27 +1,17 @@
 import { Router } from "express";
 import {
-userProfile
-}
-  from "../controllers/userController.js";
+  userProfile,
+  updateUserInfo,
+  updatePassword,
+} from "../controllers/userController.js";
 import authenticate from "../middlewares/auth.middleware.js";
-
-
-
-
 
 const router = Router();
 
-
-router.get('/profile/:id',userProfile);
-
-
-
+router.get("/profile/:id", authenticate, userProfile);
+router.patch("/:id", authenticate, updateUserInfo);
+router.patch("/:id/password" , authenticate , updatePassword);
 
 
 
-
-
-
-
-
-export default router ; 
+export default router;
