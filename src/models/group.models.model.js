791582cbb010bd8c4947e7423a77110c87da4f06
kishukumar07 <kishukumars082@gmail.com>
2025-06-21 10:35:35 +0530

    feat: created Group schema

---
+++ src/models/group.models.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const groupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: "True",
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const GroupModel = mongoose.model("Group", groupSchema);
+
+export default GroupModel;
