4777288bd90330857b23dc89caf73e8635a2acae
kishukumar07 <kishukumars082@gmail.com>
2025-06-21 12:08:22 +0530

    contest and schema model done

---
+++ src/models/story.models.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const storySchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    mediaUrl: {
+      type: String,
+      required: true, // Image or video link
+    },
+    caption: {
+      type: String,
+      maxlength: 300,
+    },
+    expiresAt: {
+      type: Date,
+      default: () => new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
+    },
+    viewers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Story", storySchema);
