457d12957c6f2a218cc5ff268a21acfcec6e6509
kishu kumar <kishukumars082@gmail.com>
2026-06-16 20:59:27 +0530

    Update token.js

--- src/utils/auth/token.js
+++ src/utils/auth/token.js
@@ -18,7 +18,7 @@ export const setRefreshToken = async(id, res) => {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,               
-    sameSite: 'None' ,
+    sameSite: 'none' ,
   };
     await RefTokenModel.create({
        token: token,
